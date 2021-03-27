a2e6aa1d7974f1b17c59bc07683517d278d13749
Rohan Goyal <rohangoyal188@gmail.com>
2021-03-27 23:38:45 +0530

    Introduction

---
+++ TASK0/Introduction/SwapTwoNumbers.java
@@ -0,0 +1,18 @@
+package TASK0.Introduction;
+
+import java.util.Scanner;
+
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Before Swap");
+        System.out.println("Enter First Number");
+        int a = sc.nextInt();
+        System.out.println("Enter Second Number");
+        int b = sc.nextInt();
+        System.out.println("After Swap");
+        System.out.println("First Number \n" + b );
+        System.out.println("Second Number \n" + a );
+
+    }
+}
