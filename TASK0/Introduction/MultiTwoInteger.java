9e47937eeab74ccd0d9e2e770eb4b9421ac40054
Rohan Goyal <rohangoyal188@gmail.com>
2021-03-27 23:38:06 +0530

    Introduction

---
+++ TASK0/Introduction/MultiTwoInteger.java
@@ -0,0 +1,16 @@
+package TASK0.Introduction;
+
+import java.util.Scanner;
+
+public class MultiTwoInteger {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the value A");
+        int A=sc.nextInt();
+        System.out.println("Enter the value B");
+        int B=sc.nextInt();
+        System.out.println("Total Multiply Sum of A and B");
+        int C= A * B ;
+        System.out.println(C);
+    }
+}
