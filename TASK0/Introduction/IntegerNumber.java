63921f53242c28ba1f6be0668b96b612c4108eff
Rohan Goyal <rohangoyal188@gmail.com>
2021-03-27 23:34:39 +0530

    Introduction

---
+++ TASK0/Introduction/IntegerNumber.java
@@ -0,0 +1,11 @@
+package TASK0.Introduction;
+
+import java.util.Scanner;
+
+public class IntegerNumber {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int a=sc.nextInt();
+        System.out.println(a);
+    }}
